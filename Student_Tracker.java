0328e990efd5a24db6a7dd98e6625cdd8f36756d
my-code-AL <110478358+my-code-AL@users.noreply.github.com>
2023-11-13 19:50:25 -0800

    Create Student_Tracker.java
    Java file that utilizes JDBC to create a reactive SQL database that works with command line input to the user terminal

---
+++ Student_Tracker.java
@@ -0,0 +1,17 @@
+# Student Course Tracker
+
+A simple Java application for tracking students and courses using a MySQL database.
+
+## Features
+- Enroll new students
+- Add new courses
+- Enroll students in courses
+- View students in a course
+- View courses for a student on a specific day
+
+## Getting Started
+Follow these instructions to set up and run the Student Course Tracker on your local machine.
+
+### Prerequisites
+- Java Development Kit (JDK)
+- MySQL database
